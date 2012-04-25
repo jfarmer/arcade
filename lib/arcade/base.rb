b0b5f8afe65ebb327065af64fc022cac82f74cc9
Jesse Farmer <jesse@20bits.com>
2012-04-25 01:58:29 -0700

    Collisons

--- lib/arcade/base.rb
+++ lib/arcade/base.rb
@@ -2,7 +2,7 @@ module Arcade
   module Keyboard
     Gosu::Button.constants.each do |cons|
       if cons =~ /^Kb/
-        self.const_set "Key" + cons.to_s.scan(/Kb(.*)/).flatten.first.to_s, 
+        self.const_set "Key" + cons.to_s.scan(/Kb(.*)/).flatten.first.to_s,
                        Gosu::Button.const_get(cons)
       end
     end
@@ -13,15 +13,15 @@ class Arcade::Color < Gosu::Color
 end
 
 class Arcade::GameWindow < Gosu::Window
-  def initialize width, height    
+  def initialize width, height
     @current_time = Gosu::milliseconds
     @objects      = []
-    
-    @keypress_listeners = Hash.new { |h,k| h[k] = [] }
+
+    @keypress_listeners  = Hash.new { |h,k| h[k] = [] }
 
     super width, height, false
   end
-    
+
   def add_object game_object
     game_object.keypress_listeners.each do |key, proc|
       self.add_listener key, game_object
@@ -29,20 +29,20 @@ class Arcade::GameWindow < Gosu::Window
 
     @objects << game_object
   end
-  
+
   def register game_object
     game_object.window = self
     self.add_object(game_object)
   end
-  
+
   def add_listener key, object
     @keypress_listeners[key] << object
   end
-  
+
   def listened_keys
     @keypress_listeners.keys
   end
-  
+
   def listeners_for_key key
     @keypress_listeners[key]
   end
@@ -52,25 +52,33 @@ class Arcade::GameWindow < Gosu::Window
       game_object.draw
     end
   end
-  
+
   def update
     dt = (Gosu::milliseconds - @current_time) / 1000.0
     @current_time = Gosu::milliseconds
-    
+
     self.listened_keys.each do |key|
       self.listeners_for_key(key).each do |listener|
         listener.key_pressed(key) if button_down?(key)
       end
-    end    
-    
+    end
+
     @objects.each do |object|
-      object.update(dt)
+      [@objects - object].each do |other|
+        if object.collides_with?(other)
+          object.collided_with(other)
+        end
+      end
+    end
+
+    @objects.each do |object|
+      object._update(dt)
     end
   end
-  
+
   def button_down id
   end
-  
+
   def draw_square upper_left, width, height, color = Gosu::Color::WHITE
     x, y = upper_left
 
@@ -84,33 +92,33 @@ end
 class Arcade::GameObject
   PROPERTIES = [:x, :y, :height, :width, :color, :name, :velocity]
   DEFAULTS   = {:color => Gosu::Color::WHITE, :velocity => Arcade::Velocity::ZERO}
-  
+
   PROPERTIES.each do |prop|
     attr_accessor prop
     alias_method :"set_#{prop}", :"#{prop}="
   end
-  
+
   alias_method :x_position, :x
   alias_method :y_position, :y
   alias_method :set_x_position, :x=
   alias_method :set_y_position, :y=
-  
+
   attr_reader :window
   attr_reader :keypress_listeners
-  
+
   class << self
     PROPERTIES.each do |prop|
       attr_accessor prop
       alias_method :"set_#{prop}", :"#{prop}="
     end
-    
+
     def config
       self
     end
 
     def set_defaults &block
       instance_eval &block
-    end    
+    end
   end
 
   def initialize &block
@@ -118,52 +126,78 @@ class Arcade::GameObject
       val = self.class.send(prop) || Arcade::GameObject::DEFAULTS[prop] || 0
       self.send(:"#{prop}=", val)
     end
-    
-    @keypress_listeners = {}
-        
+
+    @keypress_listeners  = {}
+    @collision_listeners = {}
+
     instance_exec &block
   end
-  
+
   def config
     self
   end
-  
+
   def draw
     window.draw_square [self.x, self.y], self.width, self.height, self.color
   end
-  
+
   def window= game_window
     @window = game_window
   end
-  
-  def update dt
+
+  def _update dt
     unless self.velocity.zero?
       self.x += self.velocity.element(0)
       self.y += self.velocity.element(1)
     end
+
+    if self.respond_to? :update
+      self.update(dt)
+    end
   end
-  
+
   def move_up pixels
     @y -= pixels
   end
-  
+
   def move_down pixels
     @y += pixels
   end
-  
+
   def move_left pixels
     @x -= pixels
   end
-  
+
   def move_right pixels
     @x += pixels
   end
-  
+
   def on_keypress key, &block
     @keypress_listeners[key] = block
-  end  
-  
+  end
+
   def key_pressed key
     instance_eval &@keypress_listeners[key]
+  end
+
+  def can_collide_with? klass
+    @collision_listeners.has_key? klass
+  end
+
+  def collided_with other
+    if self.can_collide_with?(other.class)
+      instance_exec other, &@collision_listeners[other.klass]
+    end
+  end
+
+  def collides_with? other
+    if !other.kind_of?(self.class) || other == self
+      false
+    else
+      !(bottom < other.top ||
+        top > other.bottom ||
+        right < other.left ||
+        left > other.right)
+    end
   end
 end
